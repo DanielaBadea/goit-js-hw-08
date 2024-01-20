68cde92fc1a6ec1d1960d15a8bce718db1302a35
DanielaBadea <badeadaniella@gmail.com>
2024-01-20 11:10:26 +0200

    solving-form

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const KEY = 'videoplayer-current-time';
+const localStorageKey = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 
 player.on('timeupdate', throttle(function (event) {
     console.log('callback called!')
-    localStorage.setItem(KEY, event.seconds);
+    localStorage.setItem(localStorageKey, event.seconds);
   }, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem(KEY)) ?? 0);
+player.setCurrentTime(localStorage.getItem(localStorageKey) ?? 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
 
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+console.log(form);
+
+const localStorageKey = "feedback-form-state";
+const localStorageEmail = localStorage.getItem(localStorageKey) ?? "" ;
+const localStorageMessage  = localStorage.getItem(localStorageKey) ?? "";
+form.elements.email.value = localStorageEmail;
+form.elements.message.value = localStorageMessage;
+console.log(localStorageEmail);
+console.log(localStorageMessage);
+
+form.addEventListener("input", throttle((evt) => {
+    console.log('Callback called!')
+    localStorage.setItem(localStorageKey, evt.target.value);
+}), 500);
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    console.log( {email: localStorageEmail, message: localStorageMessage} )
+    if(localStorageEmail === '' || localStorageMessage === ''){
+        return alert ("Input your email and feedback!");
+    }
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+});
