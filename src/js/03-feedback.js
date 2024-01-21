a1f92c9d33e51c094792547f13d303e0cdba35d6
DanielaBadea <badeadaniella@gmail.com>
2024-01-21 04:22:56 +0200

    homework-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,27 +2,47 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector(".feedback-form");
-console.log(form);
+const input = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+const btn = document.querySelector("button");
+const LOCALSTOREAGE_KEY = "feedback-form-state";
 
-const localStorageKey = "feedback-form-state";
-const localStorageEmail = localStorage.getItem(localStorageKey) ?? "" ;
-const localStorageMessage  = localStorage.getItem(localStorageKey) ?? "";
-form.elements.email.value = localStorageEmail;
-form.elements.message.value = localStorageMessage;
-console.log(localStorageEmail);
-console.log(localStorageMessage);
-
-form.addEventListener("input", throttle((evt) => {
-    console.log('Callback called!')
-    localStorage.setItem(localStorageKey, evt.target.value);
-}), 500);
-
-form.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    console.log( {email: localStorageEmail, message: localStorageMessage} )
-    if(localStorageEmail === '' || localStorageMessage === ''){
-        return alert ("Input your email and feedback!");
+const callbakSavedDataUser = () => {
+    // implementam obiectul unde utilizatorul introduce date in campurile corespuzatoare. Obiectul va avea valori goale
+    const objData = {
+        email : input.value,
+        message : textarea.value
     }
-    localStorage.removeItem(localStorageKey);
-    form.reset();
-});
+// salvam in local storage ceea ce utilizatorul va introduce, cum acesta accepta doar siruri de caractere transormam obiectul cu met JOSON.stringify
+    localStorage.setItem(LOCALSTOREAGE_KEY, JSON.stringify(objData));
+    console.log(objData);
+};
+
+// setam valorile elementelor pe baza datelor stocate in local strorage 
+if(localStorage.length != 0)
+    {
+    // input.value = JSON.parse(localStorage.getItem(LOCALSTOREAGE_KEY)).email;
+    // textarea.value = JSON.parse(localStorage.getItem(LOCALSTOREAGE_KEY)).message;
+    const formData = JSON.parse(localStorage.getItem(LOCALSTOREAGE_KEY));
+    console.log(formData);
+    if (formData)
+        {
+        input.value = formData.email ?? "";
+        textarea.value = formData.message ?? "";
+        }
+    };
+
+    form.addEventListener('input', throttle((callbakSavedDataUser), 500));
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        // const {elements: { email, message }} = evt.currentTarget;
+        console.log({email: email.value, message: message.value});
+        
+        if (email.value === "" || message === ""){
+            return alert ("Input your email and feedback!");
+        }
+        localStorage.removeItem(LOCALSTOREAGE_KEY);
+        form.reset();
+    };
+    form.addEventListener("submit", handleSubmit);
